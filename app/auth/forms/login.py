983077607913de746e23aa7c48141017c6b9a1fe
Tripa <maximax02@gmail.com>
2022-12-20 10:57:26 -0300

    - Se estiliza el formulario de login con css

--- app/auth/forms/login.py
+++ app/auth/forms/login.py
@@ -4,8 +4,8 @@ from wtforms.validators import InputRequired, Length, Email, AnyOf
 
 class LoginForm(FlaskForm):
     opcion = SelectField('Opción de logueo', choices=[('0', 'Email'), ('1', 'Número de socio')], render_kw={'onchange':'displayDiv()'})
-    email = StringField('Email')
-    num_socio = StringField('Numero de Socio')
-    password = PasswordField('Password', validators=[InputRequired()])
+    email = StringField('Email', render_kw={'class':'input', 'placeholder':' '})
+    num_socio = StringField('Numero de Socio', render_kw={'class':'input', 'placeholder':' '})
+    password = PasswordField('Password', validators=[InputRequired()], render_kw={'class':'input', 'placeholder':' '})
     remember_me = BooleanField('Recuerdame')
-    submit = SubmitField('Login')
+    submit = SubmitField('Login', render_kw={'class':'submit'})
